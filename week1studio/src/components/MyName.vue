1e25066dfe629885a2d7ebf694c174850a47facf
scho0147 <scho0147@student.moansh.edu>
2025-08-04 22:44:36 +1000

    week1studio content

---
+++ week1studio/src/components/MyName.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="myname">
+    <h1 class="green">{{ name }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.myname h1{
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .myname h1{
+    text-align: left;
+  }
+}
+</style>
