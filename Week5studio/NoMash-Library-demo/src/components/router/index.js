ef2e1fd7b5bfc1895af8a885c78ed505a9e357bb
nickchoi-monash <scho1047@student.monash.edu>
2025-08-25 20:09:20 +1000

    week4studio activity

---
+++ Week5studio/NoMash-Library-demo/src/components/router/index.js
@@ -0,0 +1,31 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ErrorView from '../views/ErrorView.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/about', name: 'About', component: AboutView, meta: { requiresAuth: true } },
+  { path: '/error', name: 'Error', component: ErrorView }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+// tiny auth checker (adjust to your app)
+function isLoggedIn() {
+  return localStorage.getItem('isAuthenticated') === 'true'
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !isLoggedIn()) {
+    next({ name: 'Error' })     // or: next({ path: '/login', query: { redirect: to.fullPath } })
+  } else {
+    next()
+  }
+})
+
+export default router
